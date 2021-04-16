85948853ff6902d42dcbb83f3b718e3042cb4bdb
kuhuhalder <kuhuhalder2701@gmail.com>
2021-04-16 13:17:51 -0400

    donut coffee

--- app/src/main/java/com/example/rucafeapp/OrderingCoffeeActivity.java
+++ app/src/main/java/com/example/rucafeapp/OrderingCoffeeActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class OrderingCoffeeActivity extends AppCompatActivity {
+public class OrderingCoffeeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner size;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordering_coffee);
+        size= findView
+
     }
 
     public void addToOrder(View view)
