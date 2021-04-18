30ec55f349910cb89a3749d78bddfa005960de8e
Dhvani K <dhvanik04@gmail.com>
2021-04-17 22:26:57 -0400

    Orders, Donuts, Store Order classes

--- app/src/main/java/com/example/rucafeapp/OrderDetailsActivity.java
+++ app/src/main/java/com/example/rucafeapp/OrderDetailsActivity.java
@@ -2,13 +2,51 @@ package com.example.rucafeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Serializable;
 
 public class OrderDetailsActivity extends AppCompatActivity {
+
+    ListView listofitems;
+    TextView subtotal,tax, total;
+    Order order=MainActivity.order; // this way or  from under placeOrder
+    StoreOrders store=MainActivity.store;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_details);
+        listofitems= findViewById(R.id.yourOrdersListView);
+        subtotal= findViewById(R.id.OrderSubtotal);
+        tax= findViewById(R.id.OrderSalesTax);
+        total = findViewById(R.id.OrderTotal);
+        subtotal.setEnabled(false);
+        tax.setEnabled(false);
+        total.setEnabled(false);
+        subtotal.setText("0.00");
+        tax.setText("0.00");
+        total.setText("0.00");
+    }
+
+    public void placeOrder(View view){
+        Intent intent= new Intent(this, MainActivity.class);
+        intent.putExtra("submittedOrder", (Parcelable) order);
+        startActivity(intent);
+//        store.add(order);
+        listofitems.clearChoices();
+
+        Context context = getApplicationContext();
+        CharSequence text = getString(R.string.order_has_been_placed);
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
--- app/src/main/java/com/example/rucafeapp/StoreOrdersActivity.java
+++ app/src/main/java/com/example/rucafeapp/StoreOrdersActivity.java
@@ -3,12 +3,24 @@ package com.example.rucafeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class StoreOrdersActivity extends AppCompatActivity {
+
+    ListView OrdersDisplay;
+    TextView OrderTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_orders);
+        OrdersDisplay=findViewById(R.id.storeOrdersListView);
+        OrderTotal=findViewById(R.id.StoreOrderTotalText);
+    }
+
+    public void deleteStoreOrder(View view){
+
     }
 }
